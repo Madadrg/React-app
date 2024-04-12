1d76bc3df241649daabdb1a9228e8d22b28e0cec
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-04-12 13:11:35 +0300

    Worked in styling

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import styles from "./Sidebar.module.css";
 import { menuConfig } from "../../helpers/menu";
-import { menuConfig } from "../../helpers/menu";
+import { MdAccountCircle} from "react-icons/md";
 
 const Sidebar = () => {
     return (
-        <div className={styles.Sidebar}>
-            <ul>
+        <aside className={styles.Sidebar}>
+            <div className={styles.sidebarBrandBox}></div>
+
+            <div className={styles.wrapperContainer}>
+               <div className={styles.listWrapper}>
                 {menuConfig.map(menuItem) => (
-                <li key={menuItem.id}>
+                <div key={menuItem.id} className={styles.text}>
                     {menuItem.icon}
-                    {menuItem.name}</li>))}
-            </ul>
-        </div>
+                    {menuItem.name}
+                </div>
+                
+                <div className={styles.userName}>
+            <span className={styles.text}>
+            <MdAccountCircle />
+                User Name
+                    </span>
+                    </div>  
+</div>
+        </aside>
     );
 };
 

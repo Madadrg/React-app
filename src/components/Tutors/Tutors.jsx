55311f34c1190f855c69a07288d92911105c9578
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-04-18 17:48:12 +0300

    Finished Tutors task

--- src/components/Tutors/Tutors.jsx
+++ src/components/Tutors/Tutors.jsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './Tutors.module.css'
 import { nanoid } from "nanoid";
 
-class Tutors = ({ tutors )} => {
-    return tutors.map({ tutor }) => {
-        constid = nanoid();
+const Tutors = ({ tutors ) } => {
+    const [showForm, setShowForm] = useState(false);
+    const [tutorsData, setTutorsData] = useState(tutors);
+
+    const renderTutorsPapers = () =>
+        tutorsData.map({ tutor }) => {
+        const id = nanoid();
         return (
         <Paper key={id}>
-            <div>
+            <div className={styles.tutorContainer}>
              <div>
             <p>{tutor.firstName}</p>
             <p>{tutor.lastName}</p>
             </div>
-            <div>
+            <div className={styles.tutorContacts}>
             <p>{tutor.phone}</p>
             <p>{tutor.email}</p>
              <p>{tutor.city}</p>
@@ -23,8 +27,33 @@ class Tutors = ({ tutors )} => {
              </div>
 </Paper>
     )};
-    };
+};
 
+const handleFormSubmit = { abstractData } => {
+    setTutorsData([...tutorsData, abstractData]);  
+};
+    
+const renderAddNewTutorForm = () => (
+    <form>
+        <input type='text' name='surname' id='surname' placeholder='Surname' />
+        <input type='text' name="name" id="name" placeholder='Name' />
+        <input type='text' name="phoneNumber" id="phoneNumber" placeholder=" Phone Number" />
+        <input type='email' name="email" id="email" placeholder="Email" />
+        <input type='text' name="city" id="city" placeholder="City" />
+        <button onClick={handleFormSubmit}>Invite</button>
+    </form>
+);
+
+return (
+<div className = { styles.wrapper }>
+    { renderTutorsPapers() }
+        {showForm ? (
+            renderAddNewTutorForm()
+        ) : (
+        <button onClick={() => setShowForm(true)}> Add Tutor</button>)}
+</div>
+);
+};
 
 Tutors.propTypes = {
   tutors: PropTypes.arrayOf(PropTypes.object),
